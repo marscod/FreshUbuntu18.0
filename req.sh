80e0058844ee14dd3283765f47214656ff490f65
Mehdi <mbahrami@ucmerced.edu>
2019-05-07 01:12:13 -0700

    Update req.sh

--- req.sh
+++ req.sh
@@ -6,3 +6,4 @@ sudo apt-get install python3-pip
 python3 -m spacy download en_core_web_sm
 git clone https://github.com/marscod/npr
 
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.1.0/en_core_web_sm-2.1.0.tar.gz
